59779183e2c23ae571d89603f9b67a8cd8a3838e
Anmol Singh <anmolsingh0914@gmail.com>
2026-06-17 02:54:33 +0530

    Completed Activity Service

---
+++ activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
@@ -0,0 +1,23 @@
+package com.fitness.activityservice.dto;
+
+import com.fitness.activityservice.model.ActivityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityRequest {
+
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+
+    private Map<String, Object> additionalMetrics;
+}
